a80d4f14aafac90148e7efe97bff3c3326aa5da3
Martina <martinaescorial9@gmail.com>
2021-06-27 15:02:52 +0200

    Add project files.

---
+++ UD5E11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UD5E11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            int suma = 0;
+
+            foreach (int i in array)
+            {
+                string posicion = "Inserta un numero en la posicion " + i;
+
+                array[i] = Convert.ToInt32(posicion);
+
+                suma =+ i;
+
+                Console.WriteLine("La suma es" + suma);
+            }
+
+
+            
+        }
+    }
+}
